d4f847afbe05f705fe63a6dc3cfbb919dac202b5
Chris Hagan <chris@stackableregiments.com>
2016-03-30 16:51:47 +1100

    cleaning up

--- go.bat
+++
@@ -1,1 +0,0 @@
-docker run -e "ERL_SSH_PORT=22" -p 8091:8091 -v /c/dev/tsung:/usr/local/tsung ddragosd/tsung-docker:latest -f /usr/local/tsung/simple.xml -k start
--- go.sh
+++ go.sh
@@ -1,1 +1,1 @@
-docker run -e "ERL_SSH_PORT=22" -p 8091:8091 -v /c/dev/tsung/:/usr/local/tsung/ ddragosd/tsung-docker:latest -f /usr/local/tsung/simple.xml start
+docker run -it stackable/tsung
